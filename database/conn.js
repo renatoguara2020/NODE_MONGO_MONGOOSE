1c4fc6420cd9021944ff26c0ce483c97d7b32272
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-15 08:12:03 -0300

    commit changes

--- database/conn.js
+++ database/conn.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const { MongoClient } = require("mongodb");
+// Connection URI
+const uri = "mongodb://localhost:27017/mongo_mongoose";
+// Create a new MongoClient
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    console.log("Conectado ao MongoDB!");
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World com Mongoose!')
-})
+run();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports = client;
