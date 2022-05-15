e773b64d946ce77ec026489c5872bc9122001914
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-15 08:45:17 -0300

    commit changes

--- model/Product.js
+++ model/Product.js
@@ -9,8 +9,8 @@ new Schema({
 
     name: {type: String, required: true},
     price:{type:Number, required: true},
-    description:{type: String, required: true}
-}),
+    description:{type: String, required: true},
+    image:{type: String, required: true},
 
 
 module.exports = Product;
